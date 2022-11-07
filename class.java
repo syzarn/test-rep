1b78167721827579df523c8b4ef120dd1a7f4139
Antor <72019681+syzarn@users.noreply.github.com>
2022-11-07 23:26:09 +0600

    Imported Java, C, Vim (Markdown) practice files

---
+++ class.java
@@ -0,0 +1,17 @@
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.print ("*\n**\n***\n****\n*****\n******\n");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        int a = 10;
+        int b = 5;
+        double pi = 3.14159;
+        double sum = a + b + pi;
+        float ans = (a * b) / (a - b);
+        System.out.println(sum);
+        System.out.println(ans);
+    }
+}
