9f6e9750b9312a8ae4db1bb200f4ed0a0e859d9b
Antor <72019681+syzarn@users.noreply.github.com>
2022-12-03 22:18:48 +0600

    Learned Escape Sequence & some Print Statements

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print ("Hello Nunu!")
+# 'Hello World' program
+print("Hello Nunu!")
+
+# Escape sequence [\]
+print("Hello \"Guddu\"\nHello \"Gwi\"")
+
+'''
+This is an example of multi-line comment
+Same thing can also be done with double quotes
+'''
+
+# Print statements [',', sep (def:[spc]), end (def:\n)]
+print("Hey,", 25, 9, sep="~", end="!\n")
+print("How are you?")
