bd8724411dc76dcf32ff871c0ff1ba0eee0545c1
Antor <72019681+syzarn@users.noreply.github.com>
2022-12-14 00:53:34 +0600

    Learned String Formatting

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-# 'Hello World' program [Importing Math Module for future usage]
+# 'Hello World' program [Importing Array, Math, NumPy, Random & Inflect Module and installing ipython, mypy & black for future usage]
+import array
 import math
+import numpy as np
+import random as ran
+import inflect
+i = inflect.engine()
 print("Hello Nunu!")
 
 # Escape sequence [\]
@@ -11,11 +16,44 @@ YES DAVID BOWIE IS THE EMBODIMENT OF SEX AND MANLYNESS, HE IS A SEX GOD AND I HA
 DAVID BOWIE IS THE GOD OF SEX. DAVID BOWIE IS ALL, BOWIE IS EVERYTHING, BOWIE IS LOVE, BOWIE IS LIFE.
 '''
 
+''' Docstrings are strings used right after the definition of a function, method, class, or module.
+    Otherwise, it's a comment.
+    Access doctrings by __doc__ attribute
+'''
+
 # Print statements [',', sep (def:[spc]), end (def:\n)]
 print("Hey,", 25, 9, sep="~", end="!\n")
 print("How are you?")
 
+# Variables
+# Variable Declaration Styles
+
+style_snake_case = "All letters of the word are lowercase but delimited by an underscore (used to name function, method)"
+styleCamelCase = "First letter of the first word in the identifier is lower case, while the first letter of every subsequent word is uppercase (rarely used in python. used in java to denote function, method, variable, propertie, attribute)"
+StylePascalCase = "First letter of every word in the identifier is uppercase (used to name class, interface*, namespace* *->in C#)"
+STYLE_SCREAMING_SNAKE_CASE = "All letters of the word are uppercase but delimited by an underscore (used to denote constant)"
+STYLEUPPERCASE = "Everything in uppercase (used to denote constant)"
+stylelowercase = "Everything in lowercase"
+_private = "Starting with an underscore (used to declare private variable)"
+
+print(style_snake_case)
+print(styleCamelCase)
+print(StylePascalCase)
+print(STYLE_SCREAMING_SNAKE_CASE)
+print(STYLEUPPERCASE)
+print(stylelowercase)
+print(_private)
+
+# Invalid Variable Declaration Styles
+
+# 259_BigDipper = "Starting with number"
+# station-to-station = "Containing hyphen"
+# in_the_flesh? = "Ending with special characters"
+# $EXHIBIT = "Starting with special characters"
+# the fragile = "Containing spaces"
+
 # Data Types
+# Primitive Data Types
 string = "mammamia"  # Text Type
 print(string, type(string))
 
@@ -25,14 +63,18 @@ print(integer, type(integer))
 float = 3.14  # Numeric Type
 print(float, type(float))
 
-π = math.pi  # Numeric Type
+π = math.pi  # Numeric Type [Math Module]
 print(π, type(π))
 
+boolean = False  # Boolean Type
+print(boolean, type(boolean))
+
+# Non-Primitive Data Types
 complex = complex(25, 9)  # Numeric Type
 print(complex, type(complex))
 
-list = ["alef", "bet", "gimel"]  # Sequence Type (Mutable)
-print(list, type(list))
+lists = ["alef", "bet", "gimel"]  # Sequence Type (Mutable)
+print(lists, type(lists))
 
 tuple = ("dalet", "he", "vav")  # Sequence Type (Immutable)
 print(tuple, type(tuple))
@@ -51,8 +93,9 @@ print(dictionary, type(dictionary))
 range = range(9)  # Sequence Type
 print(range, type(range))
 
-boolean = False  # Boolean Type
-print(boolean, type(boolean))
+array = array.array("I", [3, 6, 9])  # Sequence Type [Array Module]
+print(array, array.typecode, type(array))
+print("<typecodes:\nb-int~signchar\nB-int~unsignchar\nu-wchar_t~unicode\nh-int~signshort\nH-int~unsignshort\ni-int~signint\nI-int~unsignint\nl-int~signlong\nL-int~unsignlong\nq-int~signlonglong\nQ-int~unsignlonglong\nf-float~float\nd-double~double>")
 
 bytes = b"ninchnails"  # Binary Type
 print(bytes, type(bytes))
@@ -71,10 +114,10 @@ none = None  # None Type
 print(none, type(none))
 
 # Arithmetic Operators
-w = -25
-x = 24
-y = 11
-z = -9
+w: int = -25
+x: int = 24
+y: int = 11
+z: int = -9
 print(x + y)  # Addition
 print(x - y)  # Substraction
 print(x * y)  # Multiplication
@@ -92,3 +135,48 @@ print(math.fmod(x, y), math.fmod(x, z),
       math.fmod(w, y), math.fmod(w, z))  # Modulus (Truncated)
 print(math.remainder(x, y), math.remainder(x, z),
       math.remainder(w, y), math.remainder(w, z))  # Remainder
+
+# Number System
+print(hex(x))
+print(oct(y))
+print(bin(z))
+
+# String Formatting
+j007: str = "james"
+b007: str = "bond"
+# % operator
+print("my name is %s, %s %s" % (b007, j007, b007))
+# str.format()
+print("my name is {}, {} {}".format(b007, j007, b007))
+print("my name is {1}, {2} {0}".format(b007, b007, j007))
+print("my name is {b}, {j} {b}".format(j=j007, b=b007))
+# F-String (Literal String Interpolation)
+print(f"my name is {b007}, {j007} {b007}")
+# Practice String Formatting
+order_no = hex(ran.randint(1, 999999))
+serial_no: int = ran.randint(1, 99)
+customer_name: str = "Anupa"
+pizza_name: str = "Pineapple Pizza"
+amount: int = 1
+size: int = 16
+toppings: list = ["mushroom", "chicken", "beef", "octopus"]
+crust: str = "thin"
+extra_cheese: bool = True
+caviar: bool = True
+saffron: bool = True
+poop_sprinkles: bool = False
+price: float = 420.69
+order_details: str = f"""
+Order No. #{(order_no[2:])}
+Serial No. #{serial_no}
+Hello, {customer_name},
+We've recieved your order of {i.number_to_words(amount)} {size}" {pizza_name} with {toppings} toppings and {crust} crust. We need a bit more info about your pizza.
+Do you want extra cheese? {extra_cheese}
+Do you want Caviar? {caviar}
+Do you want Saffron? {saffron}
+Do you want Poop Sprinkles? {poop_sprinkles}
+Your bill amount is {price}$ only.
+Please pay via Cash/bKash/Card. (We'd also accept your own used panties as currency, if and only if you're a female)
+Thank you for staying with us. Never dare to come again!
+"""
+print(order_details)
---
+++ nanocalc.py
@@ -0,0 +1,2 @@
+# thought i could make a calculator with what ive learned so far
+# fuck
